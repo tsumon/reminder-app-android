22d0625353537b1aff0b0bd0f26c859b5fb9a437
tsumon <tsumonlim2016@gmail.com>
2026-08-05 18:27:12 +0800

    release: v1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -64,7 +65,7 @@ dependencies {
     // Room Database
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
 
     // WorkManager (background scheduling)
     implementation("androidx.work:work-runtime-ktx:2.9.0")
