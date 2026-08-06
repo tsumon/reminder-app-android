50eb3973348dce8d934323e40aceb83f83ec196e
tsumon <mac@MacBook-Pro.local>
2026-08-06 19:08:47 +0800

    chore: 版本号升 1.9.0(16)，发布 UI 优化+在线升级

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.reminderapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.8.7"
+        versionCode = 16
+        versionName = "1.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
