c19fb91a619a00d8ae5a058e5498b5a4818eb803
tsumon <tsumonlim2016@gmail.com>
2026-08-10 15:32:23 +0800

    release: v2.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.reminderapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 34
-        versionName = "2.0.8"
+        versionCode = 35
+        versionName = "2.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
